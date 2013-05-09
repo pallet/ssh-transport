70d5a5b9ac2b23bdafab1e370009ce194f33f8ce
Hugo Duncan <hugo@hugoduncan.org>
2013-05-09 14:54:37 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.palletops/ssh-transport "0.4.4"
+(defproject com.palletops/ssh-transport "0.4.5-SNAPSHOT"
   :description "Functions for executing scripts over ssh."
   :url "http://palletops.com"
   :license {:name "Eclipse Public License"
