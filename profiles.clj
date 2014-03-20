3f7e37084f4f37f5b429becbacbf4ffee0ae73db
Hugo Duncan <hugo@palletops.com>
2014-03-20 15:02:42 -0400

    Add travis and release config

--- profiles.clj
+++ profiles.clj
@@ -1,11 +1,15 @@
 {:dev {:dependencies [[ch.qos.logback/logback-classic "1.0.9"]]
-       :plugins [[lein-set-version "0.3.0"]]}
+       :plugins [[lein-pallet-release "0.1.2"]]
+       :pallet-release
+       {:url "https://pbors:${GH_TOKEN}@github.com/pallet/ssh-transport.git",
+        :branch "master"}}
+ :no-checkouts {:checkout-deps-shares ^{:replace true} []},
  :doc {:dependencies [[com.palletops/pallet-codox "0.1.0-SNAPSHOT"]]
        :plugins [[codox/codox.leiningen "0.6.4"]
                  [lein-marginalia "0.7.1"]]
        :codox {:writer codox-md.writer/write-docs
                :output-dir "doc/0.4/api"
-               :src-dir-uri "https://github.com/pallet/pallet-common/blob/develop"
+               :src-dir-uri "https://github.com/pallet/ssh-transport/blob/develop"
                :src-linenum-anchor-prefix "L"}
        :aliases {"marg" ["marg" "-d" "doc/0.4/annotated"]
                  "codox" ["doc"]
